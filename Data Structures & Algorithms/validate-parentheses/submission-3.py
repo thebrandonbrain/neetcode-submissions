a9c1462a75b9c8894b5ad47b4667dcc3c45f4731
thebrandonbrain <brandonsiawj@gmail.com>
2026-09-07 22:54:56 +0800

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        lis = []
+        closing = {')':'(', '}': '{', ']' : '['}
+        for parentheses in s:
+            if parentheses in closing:
+                if lis and closing[parentheses] == lis[-1]:
+                    lis.pop()
+                else:
+                    lis.append(parentheses)
+                    break
+            else:
+                lis.append(parentheses)
+
+            
+                
+        if not lis:
+            return True
+        return False
