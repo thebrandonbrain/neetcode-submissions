ac6fdce232513610ca044bf148ac3d224466ddf4
thebrandonbrain <brandonsiawj@gmail.com>
2026-09-11 22:38:06 +0800

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.py
@@ -0,0 +1,41 @@
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        if not lists:
+            return None
+
+        def mergeTwoLists(list1, list2):
+            dummy = ListNode(0)
+            curr = dummy
+
+            while list1 and list2:
+                if list1.val <= list2.val:
+                    curr.next = list1
+                    list1 = list1.next
+                else:
+                    curr.next = list2
+                    list2 = list2.next
+
+                curr = curr.next
+
+            if list1:
+                curr.next = list1
+            else:
+                curr.next = list2
+
+            return dummy.next
+
+        while len(lists) > 1:
+            merged = []
+
+            for i in range(0, len(lists), 2):
+                if i + 1 < len(lists):
+                    merged.append(
+                        mergeTwoLists(lists[i], lists[i + 1])
+                    )
+                else:
+                    merged.append(lists[i])
+
+            lists = merged
+
+        return lists[0]
